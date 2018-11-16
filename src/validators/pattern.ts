b830b986998b91cca90bbd264c802e4a8a2ebc1b
Andrei Balmus <abalmus@tacitknowledge.com>
2018-11-16 13:28:01 +0200

    Removing xregexp adding more tests

--- src/validators/pattern.ts
+++ src/validators/pattern.ts
@@ -1,5 +1,3 @@
-import * as XRegExp from 'xregexp';
-
 export default function pattern(value, regexp) {
-    return XRegExp(regexp).test(String(value));
+    return RegExp(regexp).test(String(value));
 };
